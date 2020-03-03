7185632da861ce98daa5e605c755e643206570b5
Jnfco <jnfco_18@hotmail.com>
2020-03-03 12:49:31 -0300

    conexion base de datos

--- Proyecto2FXML/src/proyecto2fxml/Proyecto2FXML.java
+++ Proyecto2FXML/src/proyecto2fxml/Proyecto2FXML.java
@@ -11,12 +11,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author jnfco
  */
 public class Proyecto2FXML extends Application {
+    
+    Connection connection;
+        String url = "jdbc:postgresql://localhost:5432/Distribuidor1";
+        String user = "postgres";
+        String password = "Distribuidos1234";
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -33,8 +44,33 @@ public class Proyecto2FXML extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Proyecto2FXML db = new Proyecto2FXML();
+         db.dbConnection();
         launch(args);
+         
     }
+    
+   public Connection dbConnection() {
+   
+    try {
+       
+    Class.forName("org.postgresql.Driver");
+   
+    } catch(ClassNotFoundException e ){
+              e.getMessage();
+    }
+   
+    try {
+       connection = DriverManager.getConnection(url, user, password);
+        JOptionPane.showMessageDialog(null, "Connected");
+    } catch (SQLException ex) {
+        Logger.getLogger(Proyecto2FXML.class.getName()).log(Level.SEVERE, null, ex);
+        JOptionPane.showMessageDialog(null, "Failed To Connect");
+    }
+   
+    return connection;
+}
+    
     
 }
